694362ca03621d2580ccb3bc674f57c181d00eaa
ahmed habeeb <az73system@gmail.com>
2019-10-19 15:32:21 +0200

    add state

---
+++ src/components/Contact.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+ class Contact extends Component {
+   state={
+     showContactInfo:true
+   };
+
+   onShowClick=(e)=>{
+     this.setState=({
+       showContactInfo:false
+
+     })
+    
+   }
+   
+  render() {
+    const {name,email,phone}= this.props.contact;
+    return (
+      <div className="card card-body mb-3">
+        <h4>{name} <i onClick={this.onShowClick} className="fas fa-sort-down"></i></h4>
+        <ul className="list-group">
+          <li className="list-group-item">{name}</li>
+          <li className="list-group-item">{email}</li>
+          <li className="list-group-item">{phone}</li>
+        </ul>
+      </div>
+    )  
+  }
+}
+
+Contact.propTypes= {
+  contact:PropTypes.object.isRequired,
+  
+
+}
+
+export default Contact;
